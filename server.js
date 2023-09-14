356698a653a5e477b444d00ad8e6c1188552df4f
KingArceus <khangtpc@gmail.com>
2023-09-14 21:20:13 +0700

    first commit

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-	const favoriteFood = process.env.FAVORITE_FOOD;
-    console.log(`My favorite food is ${favoriteFood}`);
+	console.log(`My favorite food is {$FAVORITE_FOOD}`);
     await sleep(5000);
   }
 }
